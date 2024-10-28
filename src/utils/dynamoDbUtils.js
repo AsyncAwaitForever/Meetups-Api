5ab78a99cf296fb5091ef5fa928fbe9593836308
ELSOLRA <bardakasmiljo@gmail.com>
2024-10-28 19:40:22 +0100

    updated dynamoDbUtils and simplified to have full control over all DynamoDB parameters

--- src/utils/dynamoDbUtils.js
+++ src/utils/dynamoDbUtils.js
@@ -1,61 +1,44 @@
 import {
-    DeleteCommand,
-    GetCommand,
-    PutCommand,
-    UpdateCommand,
-    QueryCommand,
-    ScanCommand,
-} from '@aws-sdk/lib-dynamodb';
+  DeleteCommand,
+  GetCommand,
+  PutCommand,
+  UpdateCommand,
+  QueryCommand,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-import dynamoDb from "../services/dynamoDb"
+import dynamoDb from "../services/dynamoDb";
 
-export const putItem = async (tableName, item) => {
-    const command = new PutCommand({
-        TableName: tableName,
-        Item: item,
-    });
-    return dynamoDb.send(command)
+export const putItem = async (params) => {
+  const command = new PutCommand(params);
+  return dynamoDb.send(command);
 };
 
-export const getItem = async (tableName, key) => {
-    const command = new GetCommand({
-        TableName: tableName,
-        Key: key,
-    });
-    return dynamoDb.send(command);
+export const getItem = async (params) => {
+  const command = new GetCommand(params);
+  return dynamoDb.send(command);
 };
 
-export const updateItem = async (tableName, key, updateExpression, expressionAttributeValues) => {
-    const command = new UpdateCommand({
-        TableName: tableName,
-        Key: key,
-        UpdateExpression: updateExpression,
-        ExpressionAttributeValues: expressionAttributeValues,
-        ReturnValues: 'UPDATED_NEW',
-    });
-    return dynamoDb.send(command);
+export const updateItem = async (params) => {
+  const command = new UpdateCommand(params);
+  return dynamoDb.send(command);
 };
 
 export const deleteItem = async (tableName, key) => {
-    const command = new DeleteCommand({
-        TableName: tableName,
-        Key: key,
-    });
-    return dynamoDb.send(command);
+  const command = new DeleteCommand({
+    TableName: tableName,
+    Key: key,
+    ReturnValues: "ALL_OLD",
+  });
+  return dynamoDb.send(command);
 };
 
-export const queryItems = async (tableName, queryParams) => {
-    const command = new QueryCommand({
-        TableName: tableName,
-        ...queryParams,
-    });
-    return dynamoDb.send(command);
+export const queryItems = async (params) => {
+  const command = new QueryCommand(params);
+  return dynamoDb.send(command);
 };
 
-export const scanItems = async (tableName, scanParams = {}) => {
-    const command = new ScanCommand({
-        TableName: tableName,
-        ...scanParams,
-    });
-    return dynamoDb.send(command);
+export const scanItems = async (params) => {
+  const command = new ScanCommand(params);
+  return dynamoDb.send(command);
 };
