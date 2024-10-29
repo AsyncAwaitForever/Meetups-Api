53b7f34c2e98ab91baac049faf0add5e2499e769
ELSOLRA <bardakasmiljo@gmail.com>
2024-10-29 18:00:16 +0100

    added listMeetups in meetupsServices, changed structure of Meetups table to meet user stories ( table still may need some correction)

--- src/handlers/Meetups/LoadData/index.js
+++ src/handlers/Meetups/LoadData/index.js
@@ -1,15 +1,15 @@
 import { loadMeetups } from "../../../services/meetupService";
 import { sendSuccessResponse, sendError } from "../../../utils/apiResponses";
 
-export const handler = async (event) => {
+export const handler = async () => {
   try {
     const result = await loadMeetups();
 
     if (!result.success) {
-        return sendError(400,{message: 'meetups already loaded'} );
+      return sendError(400, { message: "meetups already loaded" });
     }
 
-    return sendSuccessResponse(200, {message: 'meetups loaded succesfullt'})
+    return sendSuccessResponse(200, { message: "meetups loaded successfully" });
   } catch (error) {
     console.error("Error in loadMeetups handler:", error);
     return apiResponse(500, {
