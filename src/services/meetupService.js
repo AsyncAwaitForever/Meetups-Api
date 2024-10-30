029bf3c4cce77a66a5ca4d070a6e2d1d968c0094
ELSOLRA <bardakasmiljo@gmail.com>
2024-10-30 12:41:52 +0100

    added query functions to meetupService

--- src/services/meetupService.js
+++ src/services/meetupService.js
@@ -27,7 +27,7 @@ export const loadMeetups = async () => {
           host: meetup.host,
           description: meetup.description,
           availableCapacity: meetup.availableCapacity,
-          maxCapacity: meetup.maxCapacity
+          maxCapacity: meetup.maxCapacity,
         },
       };
       await dynamoDbUtils.putItem(params);
@@ -85,4 +85,100 @@ export const searchMeetups = async (keyword) => {
   } catch (error) {
     throw new Error("Database error - failed to search meetups");
   }
+};
+
+export const queryMeetupsByDate = async (date) => {
+  try {
+    const params = {
+      TableName: meetupsTable,
+      IndexName: dateIndex,
+      KeyConditionExpression: "#date = :date",
+      ExpressionAttributeNames: {
+        "#date": "date",
+      },
+      ExpressionAttributeValues: {
+        ":date": date,
+      },
+    };
+    const data = await dynamoDbUtils.queryItems(params);
+    return data.Items;
+  } catch (error) {
+    throw new Error("Database error - failed to filter/query meetups by date");
+  }
+};
+
+export const queryMeetupsByLocation = async (location) => {
+  try {
+    const params = {
+      TableName: meetupsTable,
+      IndexName: locationIndex,
+      KeyConditionExpression: "#location = :location",
+      ExpressionAttributeNames: {
+        "#location": "location",
+      },
+      ExpressionAttributeValues: {
+        ":location": location,
+      },
+    };
+    const data = await dynamoDbUtils.queryItems(params);
+    return data.Items;
+  } catch (error) {
+    throw new Error(
+      "Database error - failed to filter/query meetups by location"
+    );
+  }
+};
+
+export const queryMeetupsByCategory = async (category) => {
+  try {
+    const params = {
+      TableName: meetupsTable,
+      IndexName: categoryIndex,
+      KeyConditionExpression: "#category = :category",
+      ExpressionAttributeNames: {
+        "#category": "category",
+      },
+      ExpressionAttributeValues: {
+        ":category": category,
+      },
+    };
+    const data = await dynamoDbUtils.queryItems(params);
+    return data.Items;
+  } catch (error) {
+    throw new Error(
+      "Database error - failed to filter/query meetups by category"
+    );
+  }
+};
+
+export const queryMeetupsWithOptions = async (options) => {
+  const { date, category, location } = options;
+  let results = [];
+
+  if (date && category && location) {
+    results = await queryMeetupsByDate(date);
+    results = results.filter(
+      (meetup) => meetup.category === category && meetup.location === location
+    );
+    return results;
+  }
+
+  if (date && category) {
+    results = await queryMeetupsByDate(date);
+    results = results.filter((meetup) => meetup.category === category);
+  } else if (date && location) {
+    results = await queryMeetupsByDate(date);
+    results = results.filter((meetup) => meetup.location === location);
+  } else if (category && location) {
+    results = await queryMeetupsByCategory(category);
+    results = results.filter((meetup) => meetup.location === location);
+  } else if (date) {
+    results = await this.queryMeetupsByDate(date);
+  } else if (category) {
+    results = await this.queryMeetupsByCategory(category);
+  } else if (location) {
+    results = await this.queryMeetupsByLocation(location);
+  }
+
+  return results;
 };
