a51b61c0c821a7bd8e86ac377695fcbbc2e9814e
ELSOLRA <bardakasmiljo@gmail.com>
2024-11-01 10:27:11 +0100

    update to queryItems function

--- src/utils/dynamoDbUtils.js
+++ src/utils/dynamoDbUtils.js
@@ -36,7 +36,7 @@ export const deleteItem = async (params) => {
 export const queryItems = async (params) => {
   try {
     const command = new QueryCommand(params);
-    const response = await dynamoDbClient.send(command);
+    const response = await dynamoDb.send(command);
     return response;
   } catch (error) {
     console.error("DynamoDB Query Error:", {
